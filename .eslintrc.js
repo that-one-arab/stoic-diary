b134c6df15cb004f1065cba49510278a3d95d36a
that-one-arab <muhammed.aldulaimi98@gmail.com>
2022-03-21 10:08:00 +0300

    ESLint setup and a bit of vue config

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'prettier/prettier': ['error', { singleQuote: true }],
+    quotes: ['error', 'single', { avoidEscape: true }],
   },
 };
