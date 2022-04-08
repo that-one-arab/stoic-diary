725e7f55942bfab950e810cc3e33f04533633eb2
that-one-arab <muhammed.aldulaimi98@gmail.com>
2022-04-08 09:07:38 +0300

    Overridden "delete" keycode alias

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
+// Assign the delete key alias to only target the delete keyboard button (by default it also targets backspace button)
+Vue.config.keyCodes.delete = 46;
 
 new Vue({
   router,
