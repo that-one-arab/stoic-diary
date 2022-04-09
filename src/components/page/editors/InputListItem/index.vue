50a62c9fa74d992c2523e9e31d9dfc2ab4e1f5ba
that-one-arab <muhammed.aldulaimi98@gmail.com>
2022-04-09 14:50:19 +0300

    Implemented suggestions dropdown scrollability

--- src/components/page/editors/InputListItem/index.vue
+++ src/components/page/editors/InputListItem/index.vue
@@ -7,10 +7,11 @@
       v-bind:value="value"
       v-on:input="$emit('input', $event.target.value)"
       @click="$emit('refocus')"
-      @keyup.enter="$emit('addNewLine')"
+      @keyup.enter="handleEnter"
       @keyup.delete="$emit('removeLine', i)"
       @keyup.up="handleKeyup"
       @keyup.down="handleKeydown"
+      @keyup.esc="handleCloseSuggestionsDropdown"
     />
     <v-icon
       color="red"
@@ -19,16 +20,22 @@
     >
       mdi-close-box
     </v-icon>
-    <ul v-if="false" class="myUL">
-      <li><a class="searchbox-elements-selected" href="#">Adele</a></li>
-      <li><a class="searchbox-elements" href="#">Agnes</a></li>
-
-      <li><a class="searchbox-elements" href="#">Billy</a></li>
-      <li><a class="searchbox-elements" href="#">Bob</a></li>
-
-      <li><a class="searchbox-elements" href="#">Calvin</a></li>
-      <li><a class="searchbox-elements" href="#">Christina</a></li>
-      <li><a class="searchbox-elements" href="#">Cindy</a></li>
+    <ul
+      v-if="isFocused && suggestionDropdown.isOpen"
+      class="myUL"
+      id="suggestions-ul"
+    >
+      <li v-for="suggestion in suggestions" :key="suggestion.id">
+        <a
+          class="dropdown_elements"
+          v-bind:class="{ dropdown_elements_hovered: suggestion.hovered }"
+          href="#"
+          :id="`suggestion-${i}-${suggestion.id}`"
+          @click="scrollSuggestions(`suggestion-${i}`)"
+        >
+          {{ suggestion.value }}
+        </a>
+      </li>
     </ul>
   </div>
 </template>
@@ -45,26 +52,227 @@ export default {
   data() {
     return {
       suggestionDropdown: {
-        isOpen: false,
+        isOpen: true,
         didReturnResults: false,
       },
+      suggestions: [
+        {
+          id: 1,
+          value: 'Adele',
+          hovered: false,
+        },
+        {
+          id: 2,
+          value: 'Agnes',
+          hovered: false,
+        },
+        {
+          id: 3,
+          value: 'Billy',
+          hovered: false,
+        },
+        {
+          id: 4,
+          value: 'Bob',
+          hovered: false,
+        },
+        {
+          id: 5,
+          value: 'Calvin',
+          hovered: false,
+        },
+        {
+          id: 6,
+          value: 'Christina',
+          hovered: false,
+        },
+        {
+          id: 7,
+          value: 'Adele',
+          hovered: false,
+        },
+        {
+          id: 8,
+          value: 'Agnes',
+          hovered: false,
+        },
+        {
+          id: 9,
+          value: 'Billy',
+          hovered: false,
+        },
+        {
+          id: 10,
+          value: 'Bob',
+          hovered: false,
+        },
+        {
+          id: 11,
+          value: 'Calvin',
+          hovered: false,
+        },
+        {
+          id: 12,
+          value: 'Christina',
+          hovered: false,
+        },
+        {
+          id: 13,
+          value: 'Alice',
+          hovered: false,
+        },
+      ],
     };
   },
   computed: {
     listNumber() {
       return this.i + 1;
     },
+
+    canHoverSuggestions() {
+      const isSuggestionDropdownOpen = this.suggestionDropdown.isOpen;
+      const suggestionDropdownHasResults = this.suggestions.length;
+
+      return !!isSuggestionDropdownOpen && !!suggestionDropdownHasResults;
+    },
   },
   methods: {
     handleKeyup() {
-      if (this.isFocused) {
-        console.info('Focused!');
+      const hoveredSuggestionIndex = this.suggestions.findIndex(
+        (suggestion) => suggestion.hovered
+      );
+
+      /** If suggestions can be hovered and there is current hovered suggestion */
+      if (this.canHoverSuggestions && hoveredSuggestionIndex !== -1) {
+        /** If the current hovered element is not the first element (index 0)*/
+        if (hoveredSuggestionIndex > 0) {
+          const nextIndex = hoveredSuggestionIndex - 1;
+          this.hoverSuggestion(nextIndex, hoveredSuggestionIndex);
+
+          // Check if the newly hovered suggestion is visible
+          const hoveredSuggestionId = this.suggestions[nextIndex].id;
+          const isElVisible = this.isListElementVisible(hoveredSuggestionId);
+          if (isElVisible === false) {
+            this.scrollSuggestions('up');
+          }
+        } else {
+          /** Else if the hovered element is the first element we de-hover the element */
+          this.hoverSuggestion(null, 0);
+          /** We refocus the input element as a sanity check */
+          this.$emit('refocus');
+        }
       } else {
         this.$emit('focusAboveLine', this.i);
       }
     },
     handleKeydown() {
-      this.$emit('focusBelowLine', this.i);
+      if (this.canHoverSuggestions) {
+        const hoveredSuggestionIndex = this.suggestions.findIndex(
+          (suggestion) => suggestion.hovered
+        );
+        /** If we already have a hovered element */
+        if (hoveredSuggestionIndex !== -1) {
+          // Hover the next suggestion (below the current one)
+          const nextIndex = hoveredSuggestionIndex + 1;
+
+          // Make sure the newIndex is between the length of the suggestions array
+          if (this.suggestions.length - 1 > nextIndex) {
+            this.hoverSuggestion(nextIndex, hoveredSuggestionIndex);
+
+            // Check if the newly hovered suggestion is visible
+            const hoveredSuggestionId = this.suggestions[nextIndex].id;
+            const isElVisible = this.isListElementVisible(hoveredSuggestionId);
+            if (isElVisible === false) {
+              this.scrollSuggestions('down');
+            }
+          }
+        } else {
+          /** Else we set the first element as the current hovered element */
+          this.hoverSuggestion(0);
+        }
+      } else {
+        this.$emit('focusBelowLine', this.i);
+      }
+    },
+
+    handleCloseSuggestionsDropdown() {
+      this.suggestionDropdown.isOpen = false;
+    },
+
+    /**
+     * @example this.hoverSuggestion(null, index) => dehovers the index
+     */
+    hoverSuggestion(newIndex = null, currentIndex = null) {
+      if (newIndex !== null) this.suggestions[newIndex].hovered = true;
+      if (currentIndex !== null) this.suggestions[currentIndex].hovered = false;
+    },
+
+    handleEnter() {
+      const hoveredSuggestionIndex = this.suggestions.findIndex(
+        (suggestion) => suggestion.hovered
+      );
+      // If the suggestions dropdown is open and an element is hovered we emit take the value of the hovered el and we paste it to the input field
+      if (hoveredSuggestionIndex !== -1) {
+        const hoveredSuggestionValue =
+          this.suggestions[hoveredSuggestionIndex].value;
+        // Change the input value to the hovered suggestion value
+        this.$emit('input', hoveredSuggestionValue);
+        this.handleCloseSuggestionsDropdown();
+        this.hoverSuggestion(null, hoveredSuggestionIndex);
+      } else {
+        this.$emit('addNewLine');
+      }
+    },
+
+    scrollSuggestions(scrollDirection) {
+      // Get the suggestions dropdown container element
+      const suggestionsContainer = document.getElementById('suggestions-ul');
+
+      // Get an array of the elements in suggestions dropdown that are visible (in viewport)
+      const visibleListElements = this.suggestions.filter(
+        (suggestion) => this.isListElementVisible(suggestion.id) === true
+      );
+
+      // Get the height of one of the list elements (it doesn't matter since they are all the same height, we chose index 0)
+      const heightOfListElement = document.getElementById(
+        `suggestion-${this.i}-${visibleListElements[0].id}`
+      ).scrollHeight;
+
+      const heightOfAllVisibleElements =
+        heightOfListElement * visibleListElements.length;
+
+      if (scrollDirection === 'up')
+        // Scroll the container up using the below formula
+        suggestionsContainer.scrollTop -= heightOfAllVisibleElements;
+      else if (scrollDirection === 'down')
+        // Scroll the container down using the below formula
+        suggestionsContainer.scrollTop += heightOfAllVisibleElements;
+    },
+
+    /**
+     * @param {string} suggestionId the id of the suggestion
+     * @returns {boolean} true if the element is in viewport, false otherwise
+     */
+    isListElementVisible(suggestionId) {
+      const container = document.getElementById('suggestions-ul');
+      const focusedInputElIndex = this.i;
+      const ele = document.getElementById(
+        `suggestion-${focusedInputElIndex}-${suggestionId}`
+      );
+
+      const eleTop = ele.offsetTop;
+      const eleBottom = eleTop + ele.clientHeight;
+
+      const containerTop = container.scrollTop;
+      const containerBottom = containerTop + container.clientHeight;
+
+      // The element is fully visible in the container
+      return (
+        eleTop >= containerTop && eleBottom <= containerBottom
+        // Some part of the element is visible in the container NOT USED
+        // (eleTop < containerTop && containerTop < eleBottom) ||
+        // (eleTop < containerBottom && containerBottom < eleBottom)
+      );
     },
   },
 };
@@ -117,9 +325,9 @@ export default {
   z-index: 200;
 }
 
-.searchbox-elements {
-  border: 1px solid #ddd;
-  margin-top: -1px; /* Prevent double borders */
+.dropdown_elements {
+  border-bottom: 1px solid #ddd;
+  border-left: 1px solid #ddd;
   background-color: #f6f6f6;
   padding: 12px;
   text-decoration: none;
@@ -128,14 +336,8 @@ export default {
   display: block;
 }
 
-.searchbox-elements-selected {
-  border: 1px solid #ddd;
-  margin-top: -1px; /* Prevent double borders */
+.dropdown_elements_hovered {
   background-color: #36bcf18f;
-  padding: 12px;
-  text-decoration: none;
-  font-size: 14px;
   color: rgb(63, 6, 6);
-  display: block;
 }
 </style>
