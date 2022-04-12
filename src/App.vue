581f3832fca06fe6f162b600ba1baf07026598a7
that-one-arab <muhammed.aldulaimi98@gmail.com>
2022-04-12 20:41:23 +0300

    Feat: Added navigation bar template

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,32 @@
 <template>
   <v-app>
-    <v-main>
+    <v-app-bar app color="white" flat>
+      <v-container class="py-0 fill-height">
+        <v-avatar class="mr-10" color="grey darken-1" size="32"></v-avatar>
+
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          text
+          @click="handleRouterPush(link)"
+        >
+          {{ link }}
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive max-width="260">
+          <v-text-field
+            dense
+            flat
+            hide-details
+            rounded
+            solo-inverted
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+    <v-main class="grey lighten-3">
       <router-view />
     </v-main>
   </v-app>
@@ -11,8 +37,14 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    links: ['Home', 'Page', 'Settings', 'Log Out'],
   }),
+
+  methods: {
+    handleRouterPush(link) {
+      this.$router.push(`/${link.toLowerCase()}`);
+    },
+  },
 };
 </script>
 
