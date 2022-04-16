873d64431d9f0150b0a65dc93ab767bb815d7c70
that-one-arab <muhammed.aldulaimi98@gmail.com>
2022-04-16 12:51:36 +0300

    Feat: Conditional navbar render and extracted navbar into it's own component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="white" flat>
-      <v-container class="py-0 fill-height">
-        <v-avatar class="mr-10" color="grey darken-1" size="32"></v-avatar>
-
-        <v-btn
-          v-for="link in links"
-          :key="link"
-          text
-          @click="handleRouterPush(link)"
-        >
-          {{ link }}
-        </v-btn>
-
-        <v-spacer></v-spacer>
-
-        <v-responsive max-width="260">
-          <v-text-field
-            dense
-            flat
-            hide-details
-            rounded
-            solo-inverted
-          ></v-text-field>
-        </v-responsive>
-      </v-container>
-    </v-app-bar>
+    <app-nav-bar v-if="isNavbarDisplayed" />
     <v-main class="grey lighten-3">
       <router-view />
     </v-main>
@@ -33,16 +8,24 @@
 </template>
 
 <script>
+import AppNavBar from './components/AppNavBar';
 export default {
   name: 'App',
+  components: {
+    AppNavBar,
+  },
+  data() {
+    return {
+      isNavbarDisplayed: false,
+    };
+  },
 
-  data: () => ({
-    links: ['Dashboard', 'Page', 'Settings', 'Log Out'],
-  }),
-
-  methods: {
-    handleRouterPush(link) {
-      this.$router.push(`/${link.toLowerCase()}`);
+  watch: {
+    // On each route change
+    $route(value) {
+      // If the route is Login or Register route, remove the navbar; else display the navbar
+      if (value.name.search(/(Login|Register)/i)) this.isNavbarDisplayed = true;
+      else this.isNavbarDisplayed = false;
     },
   },
 };
---
+++ src/components/AppNavBar/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app-bar app color="white" flat>
+    <v-container class="py-0 fill-height">
+      <v-avatar class="mr-10" color="grey darken-1" size="32"></v-avatar>
+
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        text
+        @click="handleRouterPush(link)"
+      >
+        {{ link }}
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-responsive max-width="260">
+        <v-text-field
+          dense
+          flat
+          hide-details
+          rounded
+          solo-inverted
+        ></v-text-field>
+      </v-responsive>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'AppNavBar',
+
+  data: () => ({
+    links: ['Dashboard', 'Page', 'Settings', 'Log Out'],
+  }),
+
+  methods: {
+    handleRouterPush(link) {
+      this.$router.push(`/${link.toLowerCase()}`);
+    },
+  },
+};
+</script>
+
+<style></style>
